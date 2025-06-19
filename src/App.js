1d3440c8168f40b355f25203f7332cb64143e3af
Suad Muhic <58472953+dosu199@users.noreply.github.com>
2025-06-19 13:53:44 +0000

    revert build failure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import Oops from './definitelyMissing';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div>React build will fail 🚫</div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sudo,
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
