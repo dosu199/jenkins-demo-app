4ba7d95a64ba38c3aa9309ffdebaeb7c4600a45a
Suad Muhic <58472953+dosu199@users.noreply.github.com>
2025-06-20 08:42:34 +0000

    break linting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sudo,
+          Sudo
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
